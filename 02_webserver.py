afbc2d67e2582022d632ba1c23f077619c73b662
gusevna <GusevNA@yandex.ru>
2016-11-16 21:59:51 +0400

    Update 02_webserver.py
    WebServer 02 (10 clients)

--- 02_webserver.py
+++ 02_webserver.py
@@ -1,20 +1,24 @@
+import threading
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(('127.0.0.1', 2222))
-s.bind(('0.0.0.0', 2222))
-s.listen(10)
-while True:
-    conn, addr = s.accept()
-    data = conn.recv(1024)
-    if not data:
-        conn.close()
-#        break
-    else:
-        if data == b"close":
-            print ("Close connection")
-            conn.close()
-#            break
-        else:
-            print (data)
+def server_echo (sock):
+    while True:
+        conn, addr = s.accept()
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            if data == b"close":
+                #            print ("Close connection")
+                break
             conn.send(data)
-#        conn.close()
+#            print (data)
+        conn.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 2222))
+#s.bind(('0.0.0.0', 2222))
+s.listen(10)
+
+for i in range (10):
+    pserver = threading.Thread(target=server_echo, name="srv"+str (i+1), args=[s])
+    pserver.start()
+
